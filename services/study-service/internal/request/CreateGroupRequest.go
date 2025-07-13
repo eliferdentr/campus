2c5f3b95b3c7530cb8fd0c11fc052a429c075681
Elif Erden <eliferden@hitab.net>
2025-07-13 11:47:13 +0300

    rearrange

--- services/study-service/internal/request/CreateGroupRequest.go
+++
@@ -1,8 +0,0 @@
-package request
-
-type CreateGroupRequest struct {
-	Name string `json:"name" binding:"required"`
-	Description  string `json:"description,omitempty"` 
-	CourseCode  string   `json:"course_code" binding:"required"`
-    Members     []string `json:"members,omitempty"`          
-}
--- services/study-service/internal/service/group-service.go
+++
@@ -1,8 +0,0 @@
-package service
-
-import models "campus-project.com/study-service/internal/models"
-
-
-func CreateGroup (userId int64, groupData models.Group) {
-
-}
